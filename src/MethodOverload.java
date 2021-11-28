6c1a07363b796257c70e82b9662e2addb45a3527
Mike Knoles <MKnoles78@gmail.com>
2021-11-27 22:20:26 -0500

    added code to calculate square

--- src/MethodOverload.java
+++ src/MethodOverload.java
@@ -1,9 +1,23 @@
-
+//Fig. 6.10: MethodOverload.java
+//Overload method declarations
 public class MethodOverload {
 
+	// test overload square methods
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.printf("Square of integer 7 is %d%n", square(7));
+		System.out.printf("Square of double 7.5 is %f%n", square(7.5));
 	}
 
-}
+	// square method with int argument
+	public static int square(int intValue) {
+		System.out.printf("%nCalled square with in argument: %d%n", intValue);
+		return intValue * intValue;
+	}
+
+	// square method with double
+	public static double square(double doubleValue) {
+		System.out.printf("%nCalled square with double argument: %f%n", doubleValue);
+		return doubleValue * doubleValue;
+	}
+
+} // end class Method
